a72bffd4343a43249b34cda3eb198d77871be34f
youssefouali <youssef.ouali@esprit.tn>
2022-02-27 15:53:59 +0100

    Multi Commits
    *HomeComponent
    *ProductDetailsComponent
    *package react-router-dom pour le routing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,14 @@
 import React, {Component} from "react";
 import styled from "styled-components";
 export default function Home(){
-    
+
+    return (
+        <WelcomeWrapper>
+            <h1>Welcome to our shop</h1>
+        </WelcomeWrapper>
+    );
 }
+const WelcomeWrapper = styled.div `
+text-align: center;
+display: flex;
+`;
